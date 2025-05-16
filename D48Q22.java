1595b8e44d9f1d6c8468766b11c7e6fbc41c89ae
itc-s24027 <s24027@std.it-college.ac.jp>
2025-05-16 12:39:53 +0900

    certify5/16

---
+++ D48Q22.java
@@ -0,0 +1,13 @@
+import java.net.ConnectException;
+
+enum Connection {OFFLINE, ONLINE}
+
+public class D48Q22 {
+    public static void main(String[] args) {
+        Connection s = Connection.OFFLINE;
+        if (s == Connection.OFFLINE) {
+            s = Connection.ONLINE;
+        }
+        System.out.println(s);
+    }
+}
---
+++ D48Q23.java
@@ -0,0 +1,16 @@
+public class D48Q23 {
+    public static void main(String[] args) {
+        int i = 1;
+        for (;;) {
+            i = i + 3;
+            if (i < 10) {
+                System.out.println(i);
+                continue;
+            } else {
+                System.out.println(i);
+                break;
+            }
+//            System.out.println(i);
+        }
+    }
+}
