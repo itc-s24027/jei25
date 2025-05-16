d3209f26ceb842ba14653d0401ce41bdc9c1957b
itc-s24027 <s24027@std.it-college.ac.jp>
2025-05-16 11:39:18 +0900

    certify5/16

---
+++ D48Q17.java
@@ -0,0 +1,11 @@
+public class D48Q17 {
+    public static void main(String[] args) {
+        int n = 35;
+        System.out.println(n > 15 && n >= 30);
+    }
+}
+
+/*
+D48Q17について
+D48は第48回のことを表しています
+ */
