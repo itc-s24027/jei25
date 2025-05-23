30308a8f91a28b2c4038112d366cfdd480e7d5a9
itc-s24027 <s24027@std.it-college.ac.jp>
2025-05-23 12:31:23 +0900

    5/23過去問題2

---
+++ D49Q20.java
@@ -0,0 +1,7 @@
+public class D49Q20 {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+
+        System.out.println(arr.length);
+    }
+}
---
+++ D49Q21.java
@@ -0,0 +1,8 @@
+public class D49Q21 {
+    public static void main(String[] args) {
+        int n1 = 05; //8進数で5
+        int n2 = 0x10; //16進数
+
+        System.out.println(n1 * n2);
+    }
+}
