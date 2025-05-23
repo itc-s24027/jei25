b9b83d693cfe29f2f4da89b87edd2c84b9318946
itc-s24027 <s24027@std.it-college.ac.jp>
2025-05-23 11:25:30 +0900

    5/23kadai_3

---
+++ D49Q20kadai.java
@@ -0,0 +1,11 @@
+public class D49Q20kadai {
+    public static void main(String[] args) {
+        double[][] arr = new double[5][10];
+
+        System.out.println(arr[0].length);
+        System.out.println(arr[1].length);
+        System.out.println(arr[2].length);
+        System.out.println(arr[3].length);
+        System.out.println(arr[4].length);
+    }
+}
