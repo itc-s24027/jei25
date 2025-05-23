7f49b05e840713a8ad6f0f26859847519d273c1d
itc-s24027 <s24027@std.it-college.ac.jp>
2025-05-23 11:00:58 +0900

    kadai5/23

---
+++ D49Q18kadai.java
@@ -0,0 +1,9 @@
+public class D49Q18kadai {
+    public static void main(String[] args) {
+        int  total = 75;
+        int capacity = 30;
+        int cargo = 2;
+
+        System.out.println(((double)total - capacity)-((double)total / capacity * cargo));
+    }
+}
