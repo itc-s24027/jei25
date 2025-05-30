d21f6344d90f1e5697fd6339a0940b8d1fde2cc1
itc-s24027 <s24027@std.it-college.ac.jp>
2025-05-30 11:38:11 +0900

    5/30 課題D50Q20

---
+++ D50Q20kadai.java
@@ -0,0 +1,8 @@
+public class D50Q20kadai {
+    public static void main(String[] args) {
+        int num = 1000;
+        String scale = num > 1000 ? "Wide" : "Narrow";
+
+        System.out.println(scale);
+    }
+}
