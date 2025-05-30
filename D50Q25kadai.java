376adbd8633446fa8939d9864bf80d19f8135556
itc-s24027 <s24027@std.it-college.ac.jp>
2025-05-30 12:05:38 +0900

    5/30 課題D50Q25

---
+++ D50Q25kadai.java
@@ -0,0 +1,8 @@
+public class D50Q25kadai {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 8; i++) {
+            if (i % 2 == 0)
+                System.out.println(i);
+        }
+    }
+}
