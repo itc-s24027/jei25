fdf218ec6e4380fa338b5c8378ee8044aa7692d7
itc-s24027 <s24027@std.it-college.ac.jp>
2025-05-30 12:23:00 +0900

    5/30 課題D50Q27

---
+++ D50Q27kadai.java
@@ -0,0 +1,19 @@
+public class D50Q27kadai {
+    public static void main(String[] args) {
+        int control = 3;
+        String mode = "";
+
+        switch (control) {
+            case 1:
+                mode = "Warm";
+                break;
+            case 2:
+                mode = "Cool";
+                break;
+            case 3:
+                mode = "Wind";
+                break;
+        }
+        System.out.println(mode);
+    }
+}
