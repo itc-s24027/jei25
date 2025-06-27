c3fa266edd5affc68de51f27c4d2ac0ead77deb4
itc-s24027 <s24027@std.it-college.ac.jp>
2025-06-27 11:01:57 +0900

    6/27 課題D51Q19

---
+++ D51Q19kadai.java
@@ -0,0 +1,8 @@
+public class D51Q19kadai {
+    public static void main(String[] args) {
+        float f = 4.7f;
+        double d = 0.86;
+        double ans = f * d;
+        System.out.println(ans);
+    }
+}
