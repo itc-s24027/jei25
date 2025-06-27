d3a6c1f37b58c376de4be7dc01479ebb2f16aeeb
itc-s24027 <s24027@std.it-college.ac.jp>
2025-06-27 12:25:59 +0900

    6/27 課題D51Q23

---
+++ D51Q23kadai.java
@@ -0,0 +1,8 @@
+public class D51Q23kadai {
+    public static void main(String[] args) {
+        String[] a = {"春", "夏", "秋", "冬"};
+        for (String season : a) {
+            System.out.print(season + "。");
+        }
+    }
+}
