1f41a04c8332401a5a19a420962ce4db13c7e150
itc-s24027 <s24027@std.it-college.ac.jp>
2025-06-27 12:28:03 +0900

    6/27 過去問題4

---
+++ D51Q22.java
@@ -0,0 +1,9 @@
+public class D51Q22 {
+    public static void main(String[] args) {
+        view(false, "true");
+    }
+
+    public static void view(boolean b, String s) {
+        System.out.println(" " + b + s);
+    }
+}
