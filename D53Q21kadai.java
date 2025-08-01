086451d7c5f57f4aeb0d8b4b96e808fbd7fe7fa3
itc-s24027 <s24027@std.it-college.ac.jp>
2025-08-01 10:33:14 +0900

    8/1 課題D53Q21

---
+++ D53Q21kadai.java
@@ -0,0 +1,11 @@
+public class D53Q21kadai {
+    public static void main(String[] args) {
+        byte a = 0b111111;
+        byte b = 0100;
+        byte c = 0x41;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
